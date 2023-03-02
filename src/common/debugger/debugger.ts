18144ff97a7c4d10ceb25ca087b04a98a1b8d493
kabi <530853620@qq.com>
2023-03-02 17:28:28 +0800

    temp

--- src/common/debugger/debugger.ts
+++ src/common/debugger/debugger.ts
@@ -3,7 +3,9 @@ class Debugger {
         window.onerror = function (error) {
             console.error(error);
 
-            window.Modal && window.Modal.add('123')
+            window.Modal && window.Modal.add({
+                content: error.toString()
+            });
         }
     }
 };
--- src/common/modal/modal.ts
+++ src/common/modal/modal.ts
@@ -5,14 +5,16 @@ interface ModalOption {
     content: string | HTMLElement;
     onOk?: Function;
     onCancel?: Function;
+    zIndex?: number;
 };
 
 class ModalItem {
-    body: HTMLElement;
-
+    body: Element;
+    zIndex: number;
     constructor(option: ModalOption) {
+        this.zIndex = option.zIndex;
         let str = `
-            <div class="modal">
+            <div class="modal" style="z-index: ${this.zIndex};">
                 <div class="modal-content">
                 </div>
                 <div class="modal-footer">
@@ -22,6 +24,7 @@ class ModalItem {
             </div>
         `;
         let modal: Element = strToDom(str)[0];
+        this.body = modal;
         let content: HTMLDivElement = modal.querySelector('.modal-content');
         let btnConfirm: HTMLButtonElement = modal.querySelector('.modal-confirm');
         let btnCancel: HTMLButtonElement = modal.querySelector('.modal-cancel');
@@ -33,14 +36,14 @@ class ModalItem {
         }
 
         btnCancel.onclick = () => {
+            option.onCancel && option.onCancel();
             this.remove();
         };
 
         btnConfirm.onclick = () => {
+            option.onOk && option.onOk();
             this.remove();
         };
-
-        window.Modal.element.appendChild(modal);
     }
 
     remove() {
@@ -60,11 +63,14 @@ class Modal {
         this.element = document.querySelector('.modal-box');
     }
 
-    add(content: string | HTMLElement): ModalItem {
-        let item = new ModalItem({
-            content: content
-        });
+    add(option: ModalOption): ModalItem {
+        if (!('zIndex' in option)) {
+            let length = this.list.length;
+            option.zIndex = (length?this.list[length - 1].zIndex:100) + 1;
+        }
+        let item = new ModalItem(option);
         this.list.push(item);
+        this.element.appendChild(item.body);
         return item;
     }
 
@@ -76,6 +82,7 @@ class Modal {
 
     clear(): void {
         this.list = [];
+        this.element.innerHTML = '';
     }
 }
 
